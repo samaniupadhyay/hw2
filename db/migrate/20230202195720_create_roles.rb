3723ead2945f5b5491e94e2842ac22e0fdbb91b8
samaniupadhyay <samani.upadhyay@kellogg.northwestern.edu>
2023-02-02 22:16:17 +0000

    added everythign but roles

--- db/migrate/20230202195720_create_roles.rb
+++ db/migrate/20230202195720_create_roles.rb
@@ -3,7 +3,7 @@ class CreateRoles < ActiveRecord::Migration[7.0]
     create_table :roles do |t|
       t.integer "movie_id"
       t.integer "actor_id"
-      t.string "chaarcter_name"
+      t.string "character_name"
       t.timestamps
     end
   end
